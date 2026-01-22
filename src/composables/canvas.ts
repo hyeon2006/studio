06bfaa2b3fab9536747dc909d5265531f3299322
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-01-22 14:48:52 +0900

    fix: preview size error

--- src/composables/canvas.ts
+++ src/composables/canvas.ts
@@ -1,18 +1,24 @@
-import { useDevicePixelRatio, useMouseInElement, useMousePressed } from '@vueuse/core'
+import {
+    useDevicePixelRatio,
+    useElementSize,
+    useMouseInElement,
+    useMousePressed,
+} from '@vueuse/core'
 import { computed, type Ref, ref, watch, watchEffect } from 'vue'
 import { type Point, type Rect } from '../core/utils'
 
 export function useCanvas(target: Ref<HTMLElement | undefined>) {
-    const { elementX, elementY, elementWidth, elementHeight } = useMouseInElement(target)
+    const { elementX, elementY } = useMouseInElement(target)
+    const { width, height } = useElementSize(target)
     const { pressed } = useMousePressed({ target })
     const { pixelRatio } = useDevicePixelRatio()
 
     const position = computed<Point>(() => [
-        (elementX.value * 2) / elementWidth.value - 1,
-        1 - (elementY.value * 2) / elementHeight.value,
+        (elementX.value * 2) / width.value - 1,
+        1 - (elementY.value * 2) / height.value,
     ])
-    const canvasWidth = computed(() => elementWidth.value * pixelRatio.value)
-    const canvasHeight = computed(() => elementHeight.value * pixelRatio.value)
+    const canvasWidth = computed(() => width.value * pixelRatio.value)
+    const canvasHeight = computed(() => height.value * pixelRatio.value)
 
     const rect = ref<Rect>([
         [-0.5, -0.5],
@@ -29,7 +35,7 @@ export function useCanvas(target: Ref<HTMLElement | undefined>) {
             .map(([x, y], i) => [i, Math.hypot(tx - x, ty - y)] as const)
             .sort(([, a], [, b]) => a - b)[0]!
 
-        if (distance > 20 / elementWidth.value) return
+        if (distance > 20 / width.value) return
 
         return i
     })
